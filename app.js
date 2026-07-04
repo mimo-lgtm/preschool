639c0110ae287faf1bd4c7c10796f9af16452a9b
mimo-lgtm <shoji.yano@gmail.com>
2026-07-04 19:10:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -325,5 +325,6 @@ function renderStructuredIdeas(ideasDataset) {
         if (mapPillar && mapPillar.innerHTML.trim() === "") {
             mapPillar.innerHTML = `<p class="text-muted small mb-0">現在、この分野の「新統合」アイデアはありません。</p>`;
         }
+        　
     }
 }
